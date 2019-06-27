c46050a46989f50974af439a2ffbb4d8d98db0e7
Piotr Woloszun <piotr.woloszun@gmail.com>
2019-06-28 01:19:56 +0200

    ng bump up p2

--- src/app/shared/custom-material/custom-material.module.ts
+++ src/app/shared/custom-material/custom-material.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutModule } from '@angular/cdk/layout';
-import {
-  MatToolbarModule,
-  MatButtonModule,
-  MatSidenavModule,
-  MatIconModule,
-  MatListModule,
-  MatGridListModule,
-  MatCardModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatFormFieldModule,
-  MatInputModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MAT_COMPONENTS = [
   LayoutModule,
